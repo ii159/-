41d08a29a2cd265a373e41514f10f2d9e79e0629
ii59 <1692737558@.com>
2024-11-08 03:34:26 +0800

    完善内容2

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -24,6 +24,7 @@ export default defineUserConfig({
         repoLabel: "GitHub",
         repoDisplay: true,
         breadcrumb: false,
+        titleIcon: true,
 
         author: {
             name: 'linxi',
